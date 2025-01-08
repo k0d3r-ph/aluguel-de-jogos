343f6489a793dc6de433e6070a447d0f8a5ae038
Pedro Henrique <pedrohenriqueoliveira29@hotmail.com>
2025-01-08 20:44:42 -0300

    projeto inicial

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// Função para alterar os status dos jogos, junto de suas imagens
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        if (confirm("Você tem certeza que deseja devolver este jogo?")) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.textContent = 'Alugar';
+            botao.classList.remove('dashboard__item__button--return');
+        }
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');      
+    }
+}
